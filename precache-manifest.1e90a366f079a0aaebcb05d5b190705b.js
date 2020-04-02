e9b7368817ddec44f80021afae5dd8aeca68ef0d
Joshua Manalo <joshuavincemanalo@gmail.com>
2020-04-02 01:15:25 -0500

    Updates

---
+++ precache-manifest.1e90a366f079a0aaebcb05d5b190705b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb27db1b071463bafbc39f9b01b8547",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "698327d22212df9b830b",
+    "url": "/test-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f28ad73b02ffa6ea6663",
+    "url": "/test-react/static/js/2.06bd71d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react/static/js/2.06bd71d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698327d22212df9b830b",
+    "url": "/test-react/static/js/main.35fcd550.chunk.js"
+  },
+  {
+    "revision": "34713c80f72780e024de",
+    "url": "/test-react/static/js/runtime-main.07be342c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
